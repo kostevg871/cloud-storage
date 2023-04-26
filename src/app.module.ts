90119282fc0994c83541ecb0f4bc5c96a58e7849
kosteg <kostyrkoevgeniy@yandex.ru>
2023-04-26 22:15:21 +0300

    backend cloude storage

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { FilesModule } from './files/files.module';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
+
+@Module({
+  imports: [
+    UsersModule,
+    FilesModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
